099b8f6caf53ad5978b7892bed1836563c6bbeae
Kristian Freeman <kristian@simple.com>
2016-03-21 09:43:31 -0700

    Move temp files in emacs

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -8,3 +8,10 @@
 (setq org-log-done t)
 (setq org-agenda-files (list "~/Dropbox/org/kristian.org"))
 (setq-default org-startup-indented t) ;; org-mode "clean" (indented)
+
+;; http://emacsredux.com/blog/2013/05/09/keep-backup-and-auto-save-files-out-of-the-way/
+;; store all backup and autosave files in the tmp dir
+(setq backup-directory-alist
+      `((".*" . ,temporary-file-directory)))
+(setq auto-save-file-name-transforms
+      `((".*" ,temporary-file-directory t)))
