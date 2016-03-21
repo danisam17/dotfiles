8e33368cc41d7fff9383629683c11bfbaaebb21b
Kristian Freeman <kristian@simple.com>
2016-03-21 13:50:14 -0700

    more emacs plugins

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -1,3 +1,16 @@
+;; melpa setup
+;; https://melpa.org/#/getting-started
+(require 'package) ;; You might already have this line
+(add-to-list 'package-archives
+             '("melpa" . "https://melpa.org/packages/"))
+(when (< emacs-major-version 24)
+  ;; For important compatibility libraries like cl-lib
+  (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/")))
+(package-initialize) ;; You might already have this line
+
+;; set theme
+(load-theme 'tao-yin t)
+
 ;; set font
 (set-default-font "Operator Mono-14")
 
@@ -15,3 +28,23 @@
       `((".*" . ,temporary-file-directory)))
 (setq auto-save-file-name-transforms
       `((".*" ,temporary-file-directory t)))
+
+;; helm
+(require 'helm)
+(global-set-key (kbd "M-x") 'helm-M-x)
+(global-set-key (kbd "C-x C-f") 'helm-find-files)
+(global-set-key (kbd "C-x C-b") 'helm-buffers-list)
+
+;; projectile
+(require 'projectile)
+(projectile-global-mode)
+(require 'helm-projectile)
+
+;; magit
+(require 'magit)
+(global-set-key (kbd "C-x g") 'magit-status)
+
+;; http://www.ftrain.com/util_emacs_hints.html
+(defun insert-time ()
+  (interactive)
+  (insert (format-time-string "%Y-%m-%d-%R")))
